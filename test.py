6ab835de0b06bcc3955a57221ddd0d8e477e4cfb
Ivan_Duan <492987559@qq.com>
2019-04-30 10:49:40 +0800

    0

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
-a = 'jasdyegfihaewgoi'
-for i in a:
-    i = 'A'
-print(a)
-# jasdyegfihaewgoi
-print(len(a))
+# a = 'jasdyegfihaewgoi'
+# for i in a:
+#     i = 'A'
+# print(a)
+# # jasdyegfihaewgoi
+# print(len(a))
 # 16
-# for i in iter， 'i' is a copy
+# --- for i in iter， 'i' is a copy
+
+a = []
+if not a:
+    print('False')
+a = ['dsa']
+if a:
+    print('Ture')
+
+# --- 被定义为假值的常量: None 和 False。
+#     任何数值类型的零: 0, 0.0, 0j, Decimal(0), Fraction(0, 1)
+#     空的序列和多项集: '', (), [], {}, set(), range(0)
---
+++ 剑指offer/004-重建二叉树/reBuild.py
@@ -0,0 +1,29 @@
+class BiTreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.lc = None
+        self.rc = None
+
+def reBuild(pre, inorder):
+    if not pre:
+        return None
+    val = pre[0]
+    root = BiTreeNode(val)
+
+    for i in range(len(inorder)):
+        if val == inorder[i]:
+            break
+
+    root.lc = reBuild(pre[1:i+1], inorder[0:i])
+    root.rc = reBuild(pre[i+1:], inorder[i+1:])
+
+    return root
+
+def inorder(tree):
+    if tree is None:
+        return
+    inorder(tree.lc)
+    print(tree.val)
+    inorder(tree.rc)
+
+inorder(reBuild(['a', 'b', 'd', 'c'], ['d', 'b', 'a', 'c']))
