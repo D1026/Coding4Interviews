fa6b1384e6c8de14eedb7578d7230c7e61a397d1
49298 <492987559@qq.com>
2019-05-02 20:39:32 +0800

    0

--- 剑指offer/006-旋转数组的最小数字/minInRevolve.py
+++ 剑指offer/006-旋转数组的最小数字/minInRevolve.py
@@ -3,3 +3,16 @@ def getMin (array):
         return 0
     l = 0
     r = len(array) - 1
+    mid = (l+r)//2
+    while True:
+        if r == l+1:
+            return array[r]
+        if array[mid] >= array[l]:
+            l = mid
+        else:
+            r = mid
+        mid = (l+r)//2
+
+print(getMin([4, 5, 6, 1, 2]))
+print(getMin([5, 3, 4]))
+print(getMin([7, 7]))
