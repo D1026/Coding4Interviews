5e6472f2be36026726da8c12eb96ee39ac256d1d
Ivan_Duan <492987559@qq.com>
2019-05-08 15:38:02 +0800

    11※

--- 剑指offer/011-二进制中1的个数/count1.py
+++ 剑指offer/011-二进制中1的个数/count1.py
@@ -8,3 +8,15 @@ print(bin(s))
 # -0b10
 # -0b10
 # python对二进制的表示与运算进行了封装
+from ctypes import *
+
+def countOf1(x):
+    count = 0
+    while c_int(x).value:
+        print(c_int(x).value)
+        x = x & (x-1)
+        print(c_int(x).value)
+        count = count + 1
+    return count
+
+print(countOf1(-3))
