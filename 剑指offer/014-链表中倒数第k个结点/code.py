ced5437f730b478d00f8c115bb65335458d2da96
Ivan_Duan <492987559@qq.com>
2019-05-09 09:48:35 +0800

    14

--- 剑指offer/014-链表中倒数第k个结点/code.py
+++ 剑指offer/014-链表中倒数第k个结点/code.py
@@ -1,7 +1,7 @@
 class ListNode:
-    def __init__(self, x):
+    def __init__(self, x, nxt=None):
         self.val = x
-        self.next = None
+        self.next = nxt
 
 
 def FindKthToTail(head, k):
@@ -10,7 +10,7 @@ def FindKthToTail(head, k):
     fast_p = head
     slow_p = head
     for _ in range(k):
-        if fast_p:
+        if fast_p:  # 这里他用了哨兵
             fast_p = fast_p.next
         else:
             return None
@@ -18,3 +18,16 @@ def FindKthToTail(head, k):
         fast_p = fast_p.next
         slow_p = slow_p.next
     return slow_p
+
+# --- test ---
+tail = ListNode(1)
+head = ListNode(5, ListNode(4, ListNode(3, ListNode(2, tail))))
+# 遍历
+if head:
+    p = head
+    print(p.val)
+while p.next:
+    p = p.next
+    print(p.val)
+
+print('---', FindKthToTail(head, 4).val)
