4133a233c1d774598ff23b14b801d79d8631a3bf
Ivan_Duan <492987559@qq.com>
2019-05-09 16:43:10 +0800

    16

--- 剑指offer/016-合并有序链表/mergeOrd.py
+++ 剑指offer/016-合并有序链表/mergeOrd.py
@@ -33,23 +33,22 @@ def merge(list0, list1):
     p0 = list0
     p1 = list1
     mer = LinkedList()
-    while not p0 and not p1:
+    while p0 and p1:
         if p0.val > p1.val:
             mer.append(ListNode(p1.val))
             p1 = p1.next
         else:
             mer.append(ListNode(p0.val))
             p0 = p0.next
-    # --
-    traverse(p0)
-    traverse(p1)
+
     if p0:
-        mer += p0
+        mer += p0   #  这里返回self.h, mer类型被重置为 ListNode
     if p1:
         mer += p1
     return mer
 
-a = ListNode(2, ListNode(5, ListNode(7, ListNode(10, ListNode(17)))))
-b = ListNode(4, ListNode(5, ListNode(6)))
-ab = merge(a, b)
-traverse(ab)
+h0 = ListNode(2, ListNode(5, ListNode(7, ListNode(10, ListNode(17)))))
+h1 = ListNode(4, ListNode(5, ListNode(6)))
+h = merge(h0, h1)
+print(type(h))
+traverse(h)
