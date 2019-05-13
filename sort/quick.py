821f13906a83c7aac66f739132db2f38a07eb180
Ivan_Duan <492987559@qq.com>
2019-05-13 10:43:05 +0800

    0

--- sort/quick.py
+++ sort/quick.py
@@ -1,31 +1,32 @@
+import numpy as np
 def quickSort(x):
     if len(x) <= 1:
         return x
-    v = x[-1]
-    i, j = 0, len(x)-2
-    while i < j:
-        while i < j:
-            if x[i] > v:
-                break
-            else:
-                i += 1
-        while i < j:
-            if x[j] < v:
-                break
-            else:
-                j -= 1
-        x[i], x[j] = x[j], x[i]
-
-    p = i if i <= j else j
-    while p < len(x)-1:
-        if x[p] >= v:
-            x[p], x[-1] = x[-1], x[p]
+    v = x[0]
+    i, j = 1, len(x)-1
+    while True:
+        while x[i] < v and i < len(x)-1:
+            i += 1
+        while x[j] > v and j > 1:
+            j -= 1
+        if i >= j:
             break
-        else:
-            p += 1
+        x[i], x[j] = x[j], x[i]
+    print(x, 'j=', j)
+    if x[j] < v:
+        x[0], x[j] = x[j], x[0]
+        p = j
+    else:
+        x[0], x[j-1] = x[j-1], x[0]
+        p = j-1
     print(x)
     quickSort(x[:p])
     quickSort(x[p+1:])
     return x
 
-print('---', quickSort([6, 3, 2, 2, 1]))
+print('---', quickSort(np.array([13, 7, 7, 11, 7, 2, 1])))
+# --- x[a:b] 是 x 的切片， 而 a = x[a:b], a 是 x[a:b] 的一个拷贝
+# 引用作为切片存在， 必须用 numpy.array()
+
+qs = lambda xs: ((len(xs) <= 1 and [xs]) or [qs( [x for x in xs[1:] if x < xs[0]] ) + [xs[0]] + qs( [x for x in xs[1:] if x >= xs[0]] ) ] )[0]
+print('---', qs([13, 7, 7, 11, 7, 2, 1]))
--- test.py
+++ test.py
@@ -1,18 +1,32 @@
-a = 'jasdyegfihaewgoi'
-for i in a:
-    i = 'A'
-print(a)
-# jasdyegfihaewgoi
-print(len(a))
-# 16
-# for i in iter， 'i' is a copy
+# a = 'jasdyegfihaewgoi'
+# for i in a:
+#     i = 'A'
+# print(a)
+# # jasdyegfihaewgoi
+# print(len(a))
+# # 16
+# # for i in iter， 'i' is a copy
+#
+# # --- 003 ---
+# a = ['qw', 1, 4]
+# print(a.pop())
+# print(a)
+#
+# a = 1
+# while a:
+#     print('AAA')
+#     break
 
-# --- 003 ---
-a = ['qw', 1, 4]
-print(a.pop())
+# --- 5.13 ---
+def exch(x):
+    x[0], x[-1] = x[-1], x[0]
+
+import numpy as np
+a = np.arange(10)
+print(a)
+x = a[:3]
+print(x)
+exch(x)
+print(x)
 print(a)
 
-a = 1
-while a:
-    print('AAA')
-    break
