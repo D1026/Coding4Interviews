d5e36ca97fd291e80cddeec973c66b7ce4887cc0
49298 <492987559@qq.com>
2019-05-16 15:59:44 +0800

    17--

---
+++ 剑指offer/017-树的子结构/isSubTree.py
@@ -0,0 +1,13 @@
+class TreeNode:
+    def __init__(self, x, lc=None, rc=None):
+        self.val = x
+        self.lc = lc
+        self.rc = rc
+
+def isSubtree(a, b):
+
+    return
+
+"""
+遍历，碰到root2.val,递归检查结构是否相同
+"""
