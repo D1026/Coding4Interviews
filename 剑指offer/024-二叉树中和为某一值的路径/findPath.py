a0716d335fc6d2636aff618056198df076fc2015
Ivan_Duan <492987559@qq.com>
2019-06-01 10:42:40 +0800

    24

--- 剑指offer/024-二叉树中和为某一值的路径/findPath.py
+++ 剑指offer/024-二叉树中和为某一值的路径/findPath.py
@@ -1,3 +1,32 @@
 """
-参考代码里答案是错误的，没有规范路径的定义：root->leaf
+路径的定义：root->leaf
 """
+class TreeNode:
+    def __init__(self, x, lc=None, rc=None):
+        self.val = x
+        self.lc = lc
+        self.rc = rc
+
+all = []
+
+def findPath(root, n, pre=[]):
+    path = []
+    path.extend(pre)
+    if not root:
+        return
+    path.append(root.val)
+    n -= root.val
+    if n == 0 and not root.lc and not root.rc:
+        all.append(path)
+        return
+    findPath(root.lc, n, path)
+    findPath(root.rc, n, path)
+
+    return
+
+tree = TreeNode(7, TreeNode(-3, TreeNode(1), TreeNode(-1)),
+                TreeNode(1, TreeNode(0, TreeNode(-3)), TreeNode(-3)))
+
+findPath(tree, 5)
+all.sort(key=len, reverse=True)
+print(all)
