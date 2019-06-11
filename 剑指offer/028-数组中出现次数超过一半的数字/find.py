2af61b9910c9504cd3196b37c9de914a6d84a57c
49298 <492987559@qq.com>
2019-06-11 21:08:59 +0800

    0

--- 剑指offer/028-数组中出现次数超过一半的数字/find.py
+++ 剑指offer/028-数组中出现次数超过一半的数字/find.py
@@ -1,4 +1,4 @@
-# 基于快排寻找中位数 O(n), 就是寻找第 k 大的数
+# 基于快排寻找中位数 O(n), 就是寻找第 len(x)//2 大的数
 def find1(x, k):
     v = x[0]
     pre = [les for les in x if les < v]
@@ -11,6 +11,7 @@ def find1(x, k):
     else:
         return find1(post, k-len(pre+mid))
 
-a = [8, 4, 5, 4, 4, 5, 7, 4, 4]
+a = [8, 4, 5, 4, 4, 5, 7, 4, 4, 10, 4]
 print(find1(a, len(a)//2))
+
 # 对对碰方法
