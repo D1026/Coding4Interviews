583dbfd03e0e66ee63722d839a991aa8aa88fb70
Ivan_Duan <492987559@qq.com>
2019-06-13 18:02:36 +0800

    32

--- 剑指offer/032-把数组排成最小的数/buildMin.py
+++ 剑指offer/032-把数组排成最小的数/buildMin.py
@@ -1,8 +1,27 @@
 """
-借用python字符串大小比较
+整数转化成字符串
+定义大小比较：若 ab < ba, 则 a < b, ab = ba 则 a = b
+从小到大拼接
 """
+
+def compare(x, y):
+    xy = x+y
+    yx = y+x
+    if xy < yx:
+        return -1
+    if xy == yx:
+        return 0
+    if xy > yx:
+        return 1
+
 
 def build(xs):
     strs = []
     for i in xs:
         strs.append(str(i))
+    import functools
+    strs.sort(key=functools.cmp_to_key(compare))
+    return ''.join(strs)
+
+print(build([3, 32, 321]))
+print(build([13, 2, 7, 123]))
