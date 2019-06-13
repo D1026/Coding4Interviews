48790b8cba12133da7dc4de2ced5df5fddc5020d
Ivan_Duan <492987559@qq.com>
2019-06-13 10:58:38 +0800

    32-

--- 剑指offer/031-整数中1出现的次数（从1到n整数中1出现的次数）/count1.py
+++ 剑指offer/031-整数中1出现的次数（从1到n整数中1出现的次数）/count1.py
@@ -1,14 +1,24 @@
+"""
+来自编程之美的结论：
+第 i 位出现 1 的次数：(0~n-1 编号，即i位权重10^i）
+① 第 i 位为 0：由高位决定，高位数 X 10^i
+② 第 i 位为1：由高低位同时决定， (高位数 x 10^i) + 低位数+1
+③ 第 i 位 > 1: 由高位决定， (高位数+1) x 10^i
+"""
 def count(x):
-    n = len(str(x))
-    high = x % pow(10, n-1)
-    cot = 0
-    for i in range(n-1):
-        post = x % pow(10, n-1) + 1
-        pre = x // pow(10, n-i-1) + 1
-        cot += pre * post
-    cot += x % pow(10, n-1) + 1
-
-    return cot
+    counter = 0
+    num = x
+    for i in range(len(str(num))):
+        digit = num // pow(10, i) % pow(10, i+1)
+        high = num//pow(10, i+1)
+        low = num % pow(10, i)
+        if digit == 0:
+            counter += high * pow(10, i)
+        if digit == 1:
+            counter += high * pow(10, i) + low+1
+        if digit > 1:
+            counter += (high+1) * pow(10, i)
+    return counter
 
 print(count(12))
-print(count(10))
+print(count(123))
---
+++ 剑指offer/032-把数组排成最小的数/buildMin.py
@@ -0,0 +1,8 @@
+"""
+借用python字符串大小比较
+"""
+
+def build(xs):
+    strs = []
+    for i in xs:
+        strs.append(str(i))
