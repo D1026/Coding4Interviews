4dc302c59230530581e3f269073323167f648524
49298 <492987559@qq.com>
2019-06-14 22:33:58 +0800

    37-

--- 剑指offer/037-数字在排序数组中出现的次数/count.py
+++ 剑指offer/037-数字在排序数组中出现的次数/count.py
@@ -1,3 +1,31 @@
 """
 二分查找，找到头尾 i, j
 """
+def countN(x, n):
+    start, end = 0, -1
+
+    i, j = 0, len(x) - 1
+    while j > i+1:
+        ml = (i+j)//2
+        if x[ml] < n:
+            i = ml
+        if x[ml] >= n:
+            j = ml
+        if x[j] == n and (j == 0 or x[j-1] < n):
+            start = j
+            break
+
+    i, j = 0, len(x) - 1
+    while j > i + 1:
+        mr = (i + j) // 2
+        if x[mr] <= n:
+            i = mr
+        if x[mr] > n:
+            j = mr
+        if x[j] == n and (j == len(x)-1 or x[j+1] > n):
+            end = j
+            break
+
+    return (end-start+1)
+
+print(countN([1, 3, 5, 5, 7, 7, 7, 8], 7))
