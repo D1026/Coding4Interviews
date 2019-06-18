aa37fbd3e02a69e442ec8e59396c6dfb31223b1d
49298 <492987559@qq.com>
2019-06-18 13:09:07 +0800

    48

--- 剑指offer/048-不用加减乘除做加法/bitPlusOp.py
+++ 剑指offer/048-不用加减乘除做加法/bitPlusOp.py
@@ -1,14 +1,6 @@
 """
 位异或 + 位进位，直到没有进位
 """
-from ctypes import c_int
-print(bin(7))
-print(bin(7 >> 3))
-print(bin(7 & 3))
-print(bin(18))
-print(bin(7 ^ 18))
-print((7 ^ 18))
-print(c_int(7 ^ 18))
 
 
 def add(n1, n2):
@@ -18,6 +10,7 @@ def add(n1, n2):
         n2 = c_int((n1 & n2) << 1).value    # 进位
         n1 = temp
     return n1
+
 
 print(add(7, 18))
 print(add(-3, 10))
