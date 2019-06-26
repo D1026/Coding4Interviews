82f912fbaf036af7b41a581f4aadcf270d6eaac2
49298 <492987559@qq.com>
2019-06-26 22:08:03 +0800

    0

--- LeetCode2019/167. Two Sum II - Input array is sorted.py
+++ LeetCode2019/167. Two Sum II - Input array is sorted.py
@@ -18,6 +18,17 @@ Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 
 
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        i, j = 0, 0
-        
+    def twoSum(self, numbers, target):
+        i, j = 0, len(numbers)-1
+        while i < j:
+            if numbers[i]+numbers[j] == target:
+                return [i+1, j+1]
+            if numbers[i]+numbers[j] > target:
+                j -= 1
+            if numbers[i]+numbers[j] < target:
+                i += 1
+
+        return None
+
+s = Solution()
+print(s.twoSum([1,2,3,4,5], 5))
