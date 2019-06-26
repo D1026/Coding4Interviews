4cec7198e733e38db761b05e83ab9a33a07a769e
Ivan_Duan <492987559@qq.com>
2019-06-26 11:46:22 +0800

    0

--- LeetCode2019/sort/148. Sort List.py
+++ LeetCode2019/sort/148. Sort List.py
@@ -26,13 +26,72 @@ class Solution:
             fast = fast.next
         return slow
 
-    def merge(self, ):
+    def merge(self, l, r):
+
+        if l is None:
+            return r
+
+        if r is None:
+            return l
+
+        if l.val < r.val:
+            result = l
+            result.next = self.merge(l.next, r)
+        else:
+            result = r
+            result.next = self.merge(l, r.next)
+        return result
+        # if not l:
+        #     return r
+        # if not r:
+        #     return l
+        # p1, p2 = l, r
+        # pre2 = None
+        #
+        # while p1:
+        #     while p1.val > p2.val and p2.next:
+        #         pre2 = p2
+        #         p2 = p2.next
+        #
+        #     if p2.next is None:
+        #         if p1.val > p2.val:
+        #             p2.next = p1
+        #             return r
+        #         else:
+        #             p1.next = p2
+        #             return r
+        #
+        #     if pre2 is None:  # head
+        #         pre2 = p1
+        #         r = pre2
+        #         p1 = p1.next
+        #         pre2.next.next = p2
+        #
+        #     else:   # in
+        #         pre2.next = p1
+        #         p1 = p1.next
+        #         pre2.next.next = p2
+
+        # return r
 
     def sortList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        if head.next is None:
+            return head
         mid = self.mid(head)
+        print('--mid: ', mid)
         right = mid.next
         mid.next = None
-        self.sortList(head)
-        self.sortList(right)
-        return self.merge()
+        head = self.sortList(head)
+        right = self.sortList(right)
+        return self.merge(head, right)
+
+head = ListNode(-1, ListNode(5, ListNode(3, ListNode(4, ListNode(0)))))
+s = Solution()
+head = s.sortList(head)
+p = head
+while p:
+    print(p.val)
+    p = p.next
 
