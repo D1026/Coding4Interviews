eabad8b8f2a432abf3153e08cfa8692c24913ff5
Ivan_Duan <492987559@qq.com>
2019-07-04 18:46:11 +0800

    0

--- algori2019/0-1package.py
+++ algori2019/0-1package.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 
-def solve(vlist, wlist, totalWeight):
+def solve(vlist, wlist, cap):
     totalLength = len(vlist)
-    resArr = np.zeros((totalLength+1, totalWeight+1), dtype=np.int32)
+    resArr = np.zeros((totalLength+1, cap+1), dtype=np.int32)
 
     vlist.insert(0, 0)
     wlist.insert(0, 0)  # 占位，使 i, j 与 v,w-list中对齐
     for i in range(1, totalLength+1):
-        for j in range(1, totalWeight+1):
+        for j in range(1, cap+1):
             if wlist[i] <= j:
-                resArr[i, j] = max(resArr[i-1, j-wlist[i]]+vlist[i], resArr[i-1, j])
+                resArr[i, j] = max(resArr[i-1, j-wlist[i]] + vlist[i], resArr[i-1, j])
             else:
                 resArr[i, j] = resArr[i-1, j]
     return resArr[-1, -1]
@@ -19,6 +19,6 @@ def solve(vlist, wlist, totalWeight):
 if __name__ == '__main__':
     v = [60, 100, 120]
     w = [10, 20, 30]
-    weight = 50
-    result = solve(v, w, weight)
+    cap = 50
+    result = solve(v, w, cap)
     print(result)
