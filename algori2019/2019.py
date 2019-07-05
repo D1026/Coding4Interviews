db60c56491a4b8e9e22a8e4e2cc0badf7d18990f
49298 <492987559@qq.com>
2019-07-05 12:29:16 +0800

    0

--- algori2019/2019.py
+++ algori2019/2019.py
@@ -99,21 +99,21 @@ def getMin(array):
     if len(array) == 1:
         return array[0]
 
-    l = 0
-    r = len(array) - 1
-    mid = (l + r) // 2
+    left = 0
+    right = len(array) - 1
+    mid = (left + right) // 2
     while True:
-        if array[mid] >= array[l] and array[mid] <= array[r]:
+        if array[mid] >= array[left] and array[mid] <= array[right]:
             return array[0]
         # 无法处理[1,1,1,0,1]，检查是否顺序 应该搜索到底，放在while True 外面
 
-        if r == l + 1:
-            return array[r]
-        if array[mid] >= array[l]:
-            l = mid
+        if right == left + 1:
+            return array[right]
+        if array[mid] >= array[left]:
+            left = mid
         else:
-            r = mid
-        mid = (l + r) // 2
+            right = mid
+        mid = (left + right) // 2
 
 
 print(getMin([4, 5, 6, 1, 2, 3]))
