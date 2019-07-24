7d80e984c9b5629c995d2fdafab686006b4d77f8
492987559@qq.com <enjoy1911>
2019-07-24 17:17:12 +0800

    0

--- LeetCode2019/208. Implement Trie (Prefix Tree).py
+++ LeetCode2019/208. Implement Trie (Prefix Tree).py
@@ -16,7 +16,11 @@ Note:
 You may assume that all inputs are consist of lowercase letters a-z.
 All inputs are guaranteed to be non-empty strings.
 """
-
+from collections import defaultdict
+class Node:
+    def __init__(self):
+        self.children = defaultdict(Node)
+        self.isLeaf = False
 
 class Trie:
 
@@ -24,21 +28,38 @@ class Trie:
         """
         Initialize your data structure here.
         """
+        self.root = Node()
 
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
         """
+        node = self.root
+        for c in word:
+            node = node.children[c]
+        node.isLeaf = True
 
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the trie.
         """
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.isLeaf
 
     def startsWith(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
