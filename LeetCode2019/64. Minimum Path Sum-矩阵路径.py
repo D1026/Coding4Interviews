bf25d9033c25116a2db588210a89d1b04c94fc71
49298 <492987559@qq.com>
2019-08-10 22:10:03 +0800

    0

--- LeetCode2019/64. Minimum Path Sum-矩阵路径.py
+++ LeetCode2019/64. Minimum Path Sum-矩阵路径.py
@@ -17,3 +17,17 @@ Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 from typing import List
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        matrix = [[None for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    matrix[i][j] = grid[i][j]
+                elif i == 0:
+                    matrix[i][j] = grid[i][j] + matrix[i][j-1]
+                elif j == 0:
+                    matrix[i][j] = grid[i][j] + matrix[i-1][j]
+                else:
+                    matrix[i][j] = grid[i][j] + min(matrix[i][j-1], matrix[i-1][j])
+
+        return matrix[-1][-1]
