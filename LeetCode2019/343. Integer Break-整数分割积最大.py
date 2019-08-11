65293fafcf4fc2e20817c54f35a22ffbf81899e6
49298 <492987559@qq.com>
2019-08-11 22:23:58 +0800

    0

--- LeetCode2019/343. Integer Break-整数分割积最大.py
+++ LeetCode2019/343. Integer Break-整数分割积最大.py
@@ -19,4 +19,10 @@ Note: You may assume that n is not less than 2 and not larger than 58.
 
 class Solution:
     def integerBreak(self, n: int) -> int:
-        
+        products = [0, 1]
+        for i in range(2, n+1):
+            temp = 0
+            for j in range(1, i):
+                temp = max(temp, j*(i-j), j*products[i-j])
+            products.append(temp)
+        return products[-1]
